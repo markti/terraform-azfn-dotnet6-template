af4035ccaf1de42a540d793e08e791a1f725e52e
markti <markti@outlook.com>
2023-08-23 08:38:01 -0400

    add xunit test project for unit tests

---
+++ src/dotnet/GitHubCrawler/GitHubCrawler.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHubCrawler.UnitTests;
+
+public class UnitTest1
+{
+    [Trait("Category", "Unit")]
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true); // NEVER DO THIS!
+    }
+}
