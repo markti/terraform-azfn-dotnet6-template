02f1678f2b28364860e49b811700360f7d07d62e
markti <markti@outlook.com>
2023-08-24 10:52:02 -0400

    include my number in response

--- src/dotnet/GitHubCrawler/GitHubCrawler/GitHubCrawler.cs
+++ src/dotnet/GitHubCrawler/GitHubCrawler/GitHubCrawler.cs
@@ -39,7 +39,7 @@ namespace GitHubCrawler
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. My number was {myNumber}";
 
             return new OkObjectResult(responseMessage);
         }
