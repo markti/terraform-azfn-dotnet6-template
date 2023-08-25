857e738b58ed9be65b99313223a4c5daa25bfe9c
markti <markti@outlook.com>
2023-08-24 21:23:27 -0400

    add logger to the bulk service

--- src/dotnet/GitHubCrawler/GitHubCrawler.Services/BulkRequestProcessor.cs
+++ src/dotnet/GitHubCrawler/GitHubCrawler.Services/BulkRequestProcessor.cs
@@ -1,10 +1,19 @@
 ﻿using GitHubCrawler.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace GitHubCrawler.Services;
 public class BulkRequestProcessor : IBulkRequestProcessor
 {
+    private readonly ILogger<BulkRequestProcessor> _logger;
+
+    public BulkRequestProcessor(ILogger<BulkRequestProcessor> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<int> DoSomethingAsync()
     {
+        _logger.LogInformation("BulkRequestProcessor.DoSomethingAsync()");
         return 5;
     }
 }
