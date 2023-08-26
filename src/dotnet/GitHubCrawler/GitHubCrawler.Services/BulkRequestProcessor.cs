729144b9df1be4944824eba6bafaf8eacf4f973b
markti <markti@outlook.com>
2023-08-26 18:38:29 -0400

    bulk request processor app insights

--- src/dotnet/GitHubCrawler/GitHubCrawler.Services/BulkRequestProcessor.cs
+++ src/dotnet/GitHubCrawler/GitHubCrawler.Services/BulkRequestProcessor.cs
@@ -1,19 +1,24 @@
 ﻿using GitHubCrawler.Services.Interfaces;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Logging;
 
 namespace GitHubCrawler.Services;
 public class BulkRequestProcessor : IBulkRequestProcessor
 {
     private readonly ILogger<BulkRequestProcessor> _logger;
+    private readonly TelemetryClient _telemetryClient;
 
-    public BulkRequestProcessor(ILogger<BulkRequestProcessor> logger)
+    public BulkRequestProcessor(ILogger<BulkRequestProcessor> logger, TelemetryConfiguration telemetryConfiguration)
     {
         _logger = logger;
+        _telemetryClient = new TelemetryClient(telemetryConfiguration);
     }
 
     public async Task<int> DoSomethingAsync()
     {
         _logger.LogInformation("BulkRequestProcessor.DoSomethingAsync()");
+        _telemetryClient.TrackEvent("Inside the BulkRequestProcessor");
         return 5;
     }
 }
